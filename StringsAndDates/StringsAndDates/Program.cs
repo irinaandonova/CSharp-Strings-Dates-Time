1b0aa9ee9ccf54a7fdc8be11b185b645a9fc6404
irinaandonova <irinaandonova97@gmail.com>
2022-11-09 09:35:35 +0200

    Modify strings, using String methods

--- StringsAndDates/StringsAndDates/Program.cs
+++ StringsAndDates/StringsAndDates/Program.cs
@@ -6,7 +6,17 @@ namespace StringsAndDates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string greeting = "Hello, everyone";
+            string farewell = greeting.Replace("Hello", "Goodbuy");
+            string[] animals = { "cat", "dog", "rabbit", "parrot" };
+            Console.WriteLine(string.Join("\n", animals));
+            string names = "John, Melissa, Karen, Toby";
+            string [] splitNames = names.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (string name in splitNames)
+            {                
+                Console.WriteLine($"{name}");
+            }
         }
     }
 }
