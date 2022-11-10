37de99355376e6d9d6dabd597aabd93949406f91
irinaandonova <irinaandonova97@gmail.com>
2022-11-10 11:11:16 +0200

    Initialize ModifyStrings class

---
+++ StringsAndDates/StringsAndDates/ModifyStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsAndDates
+{
+    public class ModifyStrings
+    {
+        private string greeting;
+        private string farewell;
+        private string yell;
+        private DateTime startingTime;
+        private DateTime endingTime;
+
+
+    }
+}
