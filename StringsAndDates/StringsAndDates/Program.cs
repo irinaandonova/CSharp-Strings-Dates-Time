b63a495411a159fef6045ea3c6fa5ed6d840fc83
irinaandonova <irinaandonova97@gmail.com>
2022-11-10 12:16:40 +0200

    Remove useless code in Main

--- StringsAndDates/StringsAndDates/Program.cs
+++ StringsAndDates/StringsAndDates/Program.cs
@@ -6,17 +6,7 @@ namespace StringsAndDates
     {
         static void Main(string[] args)
         {
-            string greeting = "Hello, everyone";
-            string farewell = greeting.Replace("Hello", "Goodbuy");
-            string[] animals = { "cat", "dog", "rabbit", "parrot" };
-            Console.WriteLine(string.Join("\n", animals));
-            string names = "John, Melissa, Karen, Toby";
-            string [] splitNames = names.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
-            
-            foreach (string name in splitNames)
-            {                
-                Console.WriteLine($"{name}");
-            }
+           
         }
     }
 }
